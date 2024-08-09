b4436a5f27c3bae5454695a5de3aa3a1eaaef419
seregindv <seregindv@mail.ru>
2024-08-10 00:24:18 +0300

    Adding docs (for github pages)

--- build.cmd
+++ build.cmd
@@ -1,1 +1,2 @@
-.\node_modules\.bin\ng build --configuration production --base-href /gpx/
+rem .\node_modules\.bin\ng build --configuration production --base-href /gpx/
+.\node_modules\.bin\ng build --configuration production --output-path docs --base-href /gpx-converter/

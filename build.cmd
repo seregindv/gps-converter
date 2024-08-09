ce687448c902db286d11a6f659c7a53692551a36
seregindv <seregindv@mail.ru>
2024-08-10 00:30:02 +0300

    Fixing path

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
 rem .\node_modules\.bin\ng build --configuration production --base-href /gpx/
-.\node_modules\.bin\ng build --configuration production --output-path docs --base-href /gpx-converter/
+.\node_modules\.bin\ng build --configuration production --output-path docs --base-href /gps-converter/
